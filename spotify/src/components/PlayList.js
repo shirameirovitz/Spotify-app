2ea0223515c5cfc0917723be17214a0a025f4fff
Shira-Meirovitz <shirameirovi@gmail.com>
2021-04-05 16:10:02 +0300

    fixed json and playlist component

--- spotify/src/components/PlayList.js
+++ spotify/src/components/PlayList.js
@@ -1,5 +1,33 @@
-function PlayList() {
-    return(<h1>PlayList</h1>);
-  }
-  export default PlayList;
-  
+import { useEffect, useState } from "react";
+import playlistData from "../JsonFiles/playlists.json";
+
+function PlayList(props) {
+  const [exists, setExists] = useState(true);
+  const [songsList, setSongList] = useState([]);
+
+  useEffect(() => {
+    const myPlaylist = playlistData.find(
+      (item) => item.id === Number(props.match.params.id)
+    );
+    setExists(myPlaylist);
+    setSongList(myPlaylist.songsList);
+  }, []);
+  return (
+    <div>
+      <h1>Playlist</h1>
+      <div>{exists.name}</div>
+      <div>{exists.created_at}</div>
+      {console.log(songsList)}
+      <ul>
+        {" "}
+        songs:
+        {songsList.map((song, i) => {
+          return <li key={i}>{song}</li>;
+        })}
+      </ul>
+      <img src={`..${exists.cover_img}`} alt={exists.albumName}></img>
+    </div>
+  );
+}
+
+export default PlayList;
