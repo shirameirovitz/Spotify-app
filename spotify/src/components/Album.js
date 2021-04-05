0d528944455d65e5f3cad8697db21a9bda1f197a
Shira-Meirovitz <shirameirovi@gmail.com>
2021-04-05 23:12:58 +0300

    albums links to songs

--- spotify/src/components/Album.js
+++ spotify/src/components/Album.js
@@ -1,27 +1,41 @@
 import { useEffect, useState } from "react";
 import albumsData from "../JsonFiles/albums.json";
+import { Link } from "react-router-dom";
+import songsData from "../JsonFiles/songs.json";
 
 function Album(props) {
   const [exists, setExists] = useState(true);
   const [songsList, setSongList] = useState([]);
+  const [songId, setSongId] = useState();
 
   useEffect(() => {
     const myAlbum = albumsData.find(
       (item) => item.id === Number(props.match.params.id)
     );
     setExists(myAlbum);
-    console.log(myAlbum);
     setSongList(myAlbum.songsList);
   }, []);
+  {
+    console.log(songsList);
+  }
+
   return (
     <div>
       <h1>Album</h1>
       <div>{exists.albumName}</div>
-      <div>{exists.artistName}</div>
-      {console.log(songsList)}
+      <div>Artist:{exists.artistName}</div>
       <ol>
+        <h2>songs</h2>
         {songsList.map((song, i) => {
-          return <li key={i}>{song}</li>;
+          const mySong = songsData.find((item) => item.songName === song);
+          console.log(mySong);
+          // setSongId(mySong);
+          console.log(songId);
+          return (
+            <Link to={`/song/${mySong.id}?album=${exists.id}`}>
+              <li key={i}>{song}</li>
+            </Link>
+          );
         })}
       </ol>
       <img src={`..${exists.cover_img}`} alt={exists.albumName}></img>
--- spotify/src/components/Artist.js
+++ spotify/src/components/Artist.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 function Artist(props) {
   const [exists, setExists] = useState(true);
   const [songSelected, setSongSelected] = useState([]);
+  const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
     const myArtist = artistsData.find(
@@ -11,18 +12,27 @@ function Artist(props) {
     );
     setExists(myArtist);
     console.log(myArtist);
-    setSongSelected(myArtist.songSelected);
+    setSongSelected(myArtist.selectedSong);
+    setAlbums(myArtist.albumsList);
   }, []);
   return (
     <div>
       <h1>Artist</h1>
       <div>{exists.name}</div>
-      <img>src={exists.albumsList}</img>
+
       <ol>
+        <h2>Albums</h2>
+        {albums.map((album, i) => {
+          return <li key={i}>{album}</li>;
+        })}
+      </ol>
+      <ol>
+        <h2>Selected songs</h2>
         {songSelected.map((song, i) => {
           return <li key={i}>{song}</li>;
         })}
       </ol>
+
       <img src={`..${exists.cover_img}`} alt={exists.name}></img>
       <ul>{songSelected}</ul>
     </div>
