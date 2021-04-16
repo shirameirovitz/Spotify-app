b6b3d8995d9fb37d59fa084ccdc6dff68c24be97
Shira-Meirovitz <shirameirovi@gmail.com>
2021-04-16 16:54:41 +0300

    get song request

---
+++ serverSQL/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+app.use(express.json());
+
+let mysqlCon = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'shira1999',
+  database: 'spotify',
+  multipleStatements: true,
+});
+
+mysqlCon.connect((err) => {
+  if (err) throw err;
+  console.log('connected!');
+});
+
+app.get('/songs', (req, res) => {
+  mysqlCon.query(
+    'SELECT * FROM songs ORDER BY views DESC LIMIT 5',
+    (err, results, fields) => {
+      if (err) {
+        res.send(err.message);
+      }
+      res.send(results);
+    }
+  );
+});
+
+// app.get('/songs/:id', (req, res) => {
+//   mysqlCon.query('SELECT * FROM songs WHERE id = ? AND Plays > ?', [req.params.id, 100], (err, results, fields) => {
+//       console.log(fields);
+//       if(err){
+//           res.send(err.message);
+//       } else if (results && results.length === 1){
+//           res.send(results[0]);
+//       } else {
+//           res.send(results);
+//       }
+//   });
+// });
+
+app.listen(3001);
